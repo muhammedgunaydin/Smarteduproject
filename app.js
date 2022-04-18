0fc7d701dbe40e4204e84cdafad8a8819815d90e
Muhammed <muhammed-gunaydin@hotmail.com>
2022-04-18 17:58:47 +0300

    Organised ejs files

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require('express')
 
 const app = express()
 
+//Template engine
+app.set("view engine", "ejs")
+
+//Middlewares
+app.use(express.static("public"))
+
+//routes
 app.get('/', (req, res) => {
-    res.status(200).send('/index')
+    res.status(200).render('index', {
+        page_name: "index"
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    })
 })
 
 const port = 3000

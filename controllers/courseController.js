f7b55b539d1663239cfdb327937f62874c70d5cb
Muhammed <muhammed-gunaydin@hotmail.com>
2022-04-22 03:50:41 +0300

    fix patch

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,17 +1,37 @@
 const Course = require('../models/Course')
 
 exports.createCourse = async (req, res) => {
-    
+
 
     try {
         const courses = await Course.create(req.body)
+
         res.status(201).json({
             status: 'success',
             courses
-            
+
         })
 
-    } catch(error){
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error
+        })
+    }
+
+}
+
+exports.getAllCourses = async (req, res) => {
+
+    try {
+        const courses = await Course.find()
+
+        res.status(200).render('courses',{
+            courses,
+            page_name:'courses'
+        })
+
+    } catch (error) {
         res.status(400).json({
             status: 'fail',
             error

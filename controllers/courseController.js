696bd7c4acc8aad433288afb80db4a345f2669f9
Muhammed <muhammed-gunaydin@hotmail.com>
2022-04-29 21:42:14 +0300

    Added search and bug fix

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -31,6 +31,7 @@ exports.getAllCourses = async (req, res) => {
     try {
 
         const categorySlug = req.query.categories
+        const query = req.query.search
 
         const category = await Category.findOne({ slug: categorySlug })
 
@@ -40,7 +41,21 @@ exports.getAllCourses = async (req, res) => {
             filter = { category: category._id }
         }
 
-        const courses = await Course.find(filter).sort('-createdAt')
+        if (query) {
+            filter = { name: query }
+        }
+
+        if (!query && !categorySlug) {
+            filter.name = "",
+                filter.category
+        }
+
+        const courses = await Course.find({
+            $or: [
+                { name: { $regex: '.*' + filter.name + '.*', $options: 'i' } },
+                { category: filter.category }
+            ]
+        }).sort('-createdAt').populate('user')
 
         const categories = await Category.find()
 
@@ -64,11 +79,13 @@ exports.getCourse = async (req, res) => {
     try {
         const user = await User.findById(req.session.userID)
         const course = await Course.findOne({ slug: req.params.slug }).populate('user')
+        const categories = await Category.find()
 
         res.status(200).render('course', {
             course,
             page_name: 'courses',
-            user
+            user,
+            categories
 
         })
 
